c08f11a8469a35612df4d9364eee84104c42ac00
MaxPlay <maxplayyt@gmail.com>
2020-07-10 23:01:11 +0200

    Cleaned Game1, renamed it and set it up for usage

---
+++ GmtkJam2020/GameCore.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GmtkJam2020
+{
+    public class GameCore : Game
+    {
+        public static GameCore Instance { get; private set; }
+
+        public GraphicsDeviceManager Graphics { get; private set; }
+
+        public SpriteBatch SpriteBatch { get; private set; }
+
+
+
+        public GameCore()
+        {
+            Instance = this;
+            Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+        
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            base.Draw(gameTime);
+        }
+    }
+}
--- GmtkJam2020/Program.cs
+++ GmtkJam2020/Program.cs
@@ -14,7 +14,7 @@ namespace GmtkJam2020
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GameCore())
                 game.Run();
         }
     }
